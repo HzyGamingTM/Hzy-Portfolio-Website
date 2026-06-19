fedda5b096c9071670e94656f43ea5956737da70
Hzygamingtm <zhengyonghoe@gmail.com>
2026-06-19 16:30:01 +0800

    a

--- server.ts
+++ server.ts
@@ -45,3 +45,17 @@ export default async function handler(req: IncomingMessage, res: ServerResponse)
   await app.ready();
   app.server.emit("request", req, res);
 }
+
+if (process.env.NODE_ENV !== "production") {
+  const start = async () => {
+    try {
+      await app.listen({ port: 3000 });
+      console.log("Local server running on http://localhost:3000");
+    } catch (err) {
+      app.log.error(err);
+      process.exit(1);
+    }
+  };
+
+  start();
+}
